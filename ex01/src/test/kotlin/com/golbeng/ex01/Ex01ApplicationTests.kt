a5aecbdc9d7e30d8dd22a0439c473374ba29d0b2
June <golbeng0@gmail.com>
2024-03-30 20:58:39 +0900

    스프링 프레임워크 연습 공간

---
+++ ex01/src/test/kotlin/com/golbeng/ex01/Ex01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.golbeng.ex01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ex01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
